1ea0992e6f14233d96823c5a4973e535f39d5e09
Braedon <Braedonww@gmail.com>
2018-10-31 21:07:05 +1100

    Update notes.rb

--- Formula/notes.rb
+++ Formula/notes.rb
@@ -9,8 +9,10 @@ class Notes < Formula
   
   def install
     bin.mkpath
-    bin.install "notes.sh", "notes"
-    bin.install "daemon_notes.sh", "daemon_notes"
+    bin.install "notes.sh"
+    bin.install "daemon_notes.sh"
+    mv bin/"notes.sh", bin/"notes"
+    mv bin/"daemon_notes.sh", bin/"daemon_notes"
     mkdir_p share/"notes_data"
     mv "style.css", share/"notes_data"
     mv "template.html", share/"template.html"
