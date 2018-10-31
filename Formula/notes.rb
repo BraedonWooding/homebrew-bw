ad5becf4a62b5bcd7e3c30207eb5dc049caeb70b
Braedon <Braedonww@gmail.com>
2018-10-31 20:51:04 +1100

    Updated formula

--- Formula/notes.rb
+++ Formula/notes.rb
@@ -2,11 +2,16 @@ class Notes < Formula
   desc "A simple note converting application"
   head "https://github.com/BraedonWooding/notes.git"
 
+  head do
+    url "https://github.com/BraedonWooding/notes.git"
+    depends_on "pandoc" => :build
+  end
+  
   def install
-    system "./install"
+    system "./install.sh"
   end
   
   def uninstall
-    system "./uninstall"
+    system "./uninstall.sh"
   end
 end
